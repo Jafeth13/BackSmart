6513b1fb836998c7967622720fa4e7978ad4d130
Jafeth13 <81998303+Jafeth13@users.noreply.github.com>
2022-07-06 18:05:00 -0600

    new
    new

--- Proyecto/src/main/java/com/SmartParking/Proyect/controller/ParkingController.java
+++ Proyecto/src/main/java/com/SmartParking/Proyect/controller/ParkingController.java
@@ -6,10 +6,7 @@ import com.SmartParking.Proyect.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -21,6 +18,7 @@ public class ParkingController {
     @Autowired
     private ParkingService service;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/getAll")
     public List<ParkingLot> list() {
         return service.listAll();
